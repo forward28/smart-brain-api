a1c963206c2e75dac410f997baaa3ba9d6c80db6
forward28 <forward28@inbox.lv>
2023-12-13 19:23:42 +0200

    adding port

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.post("/imageurl", (req, res) => {
 //   res.status(400).json("not found");
 // }
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is running on port ${process.env.PORT}");
 });
