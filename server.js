b7083c96d6f23eb65d62e21d0ec416ace39334de
forward28 <forward28@inbox.lv>
2024-03-24 14:29:53 +0200

    adding database conntction

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-cubic-24323",
-    //port: 5432,
-    user: "postgres",
-    password: "Nataha1(())",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
